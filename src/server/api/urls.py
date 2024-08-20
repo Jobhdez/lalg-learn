4d403f6c075136da187b3983ca61a1c42d9f9943
Job Hernandez <hj93@protonmail.com>
2024-08-20 11:01:55 -0700

    add urls for compilation

--- src/server/api/urls.py
+++ src/server/api/urls.py
@@ -6,4 +6,6 @@ app_name="api"
 urlpatterns=[
     path("vector/", views.evaluate_vector_expression, name="vector"),
     path("matrix/", views.evaluate_matrix_expression, name="matrix"),
+    path("c/matrix/", views.compile_matrix_expression, name="cmatrix"),
+    path("c/vector/", views.compile_vector_expression, name="cvector"),
     ]
