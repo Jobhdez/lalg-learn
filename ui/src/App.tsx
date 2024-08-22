a704237009e1db6386fcf0eedd423803699b8665
Job Hernandez <hj93@protonmail.com>
2024-08-22 10:04:19 -0700

    add header component

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,9 +4,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import InputExpression from "./components/InputExpression";
 import MatrixExpression from "./components/MatrixExpression";
 import Home from "./components/Home";
+import Header from "./components/Header";
+
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/matrix" element={<MatrixExpression />} />
